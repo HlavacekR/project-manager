e943e096ba4e019108f87770ccc088bd7f0ebffb
Mephisto323 <Mephi@email.cz>
2017-07-18 12:13:17 +0200

    nette libraries

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault()
+    {
+        $this->template->projects = $this->database->table('project')
+            ->order('id DESC');
+    }
+}
