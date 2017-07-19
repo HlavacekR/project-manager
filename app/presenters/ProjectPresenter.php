0e53510db3165c9c3f4b37758d1ca79eeef7b856
Mephisto323 <Mephi@email.cz>
2017-07-19 13:34:56 +0200

    update bootstrap renderer

--- app/presenters/ProjectPresenter.php
+++ app/presenters/ProjectPresenter.php
@@ -36,28 +36,29 @@ class ProjectPresenter extends Nette\Application\UI\Presenter
         $form->onSuccess[] = [$this, 'postFormSucceeded'];
 
 
-
-        // setup form rendering
+        //bootstrap 3 renderer
         $renderer = $form->getRenderer();
-        $renderer->wrappers['controls']['container'] = NULL;
+        $renderer->wrappers['controls']['container'] = null;
         $renderer->wrappers['pair']['container'] = 'div class=form-group';
         $renderer->wrappers['pair']['.error'] = 'has-error';
         $renderer->wrappers['control']['container'] = 'div class=col-sm-9';
         $renderer->wrappers['label']['container'] = 'div class="col-sm-3 control-label"';
         $renderer->wrappers['control']['description'] = 'span class=help-block';
         $renderer->wrappers['control']['errorcontainer'] = 'span class=help-block';
-// make form and controls compatible with Twitter Bootstrap
         $form->getElementPrototype()->class('form-horizontal');
-        foreach ($form->getControls() as $control) {
-            if ($control instanceof Controls\Button) {
-                $control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-success' : 'btn btn-default');
-                $usedPrimary = TRUE;
-            } elseif ($control instanceof Controls\TextBase || $control instanceof Controls\SelectBox || $control instanceof Controls\MultiSelectBox) {
-                $control->getControlPrototype()->addClass('form-control');
-            } elseif ($control instanceof Controls\Checkbox || $control instanceof Controls\CheckboxList || $control instanceof Controls\RadioList) {
-                $control->getSeparatorPrototype()->setName('div')->addClass($control->getControlPrototype()->type);
+        $form->onRender[] = function ($form) {
+            foreach ($form->getControls() as $control) {
+                $type = $control->getOption('type');
+                if ($type === 'button') {
+                    $control->getControlPrototype()->addClass(empty($usedPrimary) ? 'btn btn-success' : 'btn btn-default');
+                    $usedPrimary = true;
+                } elseif (in_array($type, ['text', 'textarea', 'select'], true)) {
+                    $control->getControlPrototype()->addClass('form-control');
+                } elseif (in_array($type, ['checkbox', 'radio'], true)) {
+                    $control->getSeparatorPrototype()->setName('div')->addClass($type);
+                }
             }
-        }
+        };
 
 
         return $form;
